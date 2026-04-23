b421f8545f873685bd4a51f1bbf57d8d4da39e06
SAKSHI G KULKARNI <sgkulkarni03@gmail.com>
2026-04-23 13:22:33 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,25 @@
+import java.util.Scanner;
 public class Calculator {
+    public static void main(String[] args){
+        Scanner sc = new Scanner()
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public int divide(int a, int b) {
-        if (b == 0) throw new IllegalArgumentException("Divide by zero");
-        return a / b;
+        public int add(int a, int b) {
+            return a + b;
+        }
+    
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+    
+        public int multiply(int a, int b) {
+            return a * b;
+        }
+    
+        public int divide(int a, int b) {
+            if (b == 0) throw new IllegalArgumentException("Divide by zero");
+            return a / b;
+        }
     }
 }
