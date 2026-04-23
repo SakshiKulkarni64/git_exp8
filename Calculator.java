5466003bc569f0882ee9a4bcb0ddb7f7e99a4bc1
SAKSHI G KULKARNI <sgkulkarni03@gmail.com>
2026-04-23 13:14:10 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,29 +1,19 @@
-public class Calculator{
- 
-    
-  public static int add {
-    int a;
-    int b;
-    return int a+b;
-  }
-  public static int subtract {
-    int a;
-    int b;
-    return int a-b;
-  }
-  public static int multiply {
-    int a;
-    int b;
-    return int a*b;
-  }
-  public static int divide {
-    int a;
-    int b;
-    if b==0{
-      ThrowException("cannot divide by zero")}
-    else{
-      return int a/b;
-   
-  }
-  
-  
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Divide by zero");
+        return a / b;
+    }
+}
