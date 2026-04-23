ba4b29e697659d9e6e6f7f43085987cc2db145b9
SAKSHI G KULKARNI <sgkulkarni03@gmail.com>
2026-04-23 13:04:06 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator{
+ 
+    
+  public static int add {
+    int a;
+    int b;
+    return int a+b;
+  }
+  public static int subtract {
+    int a;
+    int b;
+    return int a-b;
+  }
+  public static int multiply {
+    int a;
+    int b;
+    return int a*b;
+  }
+  public static int divide {
+    int a;
+    int b;
+    if b==0{
+      ThrowException("cannot divide by zero")}
+    else{
+      return int a/b;
+   
+  }
+  
+  
